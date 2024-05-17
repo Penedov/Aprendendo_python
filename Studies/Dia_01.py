f15fa11481bd3cbd5bdef14f1487b7eff85a471e
Vinicius Penedo <Viniciuspenedo2003@gmail.com>
2024-05-17 20:02:04 -0300

    Dia 01: Uma breve introdução

---
+++ Studies/Dia_01.py
@@ -0,0 +1,15 @@
+#Introdução a Python, Dia 01
+#Instruções Basícas
+print("Hello World")
+print('Hello World')
+
+#Tipos de Variáveis: é possivel criar com especificação de tipo ou não
+
+# Os nomes das variáveis diferenciam maiúsculas de minúsculas (idade, Idade e IDADE são três variáveis diferentes)
+
+a = str(3)
+b = int(3)
+c = float(3)
+A = 3
+
+print(type(a),type(b),type(c),type(A))
