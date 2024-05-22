29d43b7e22291e25bdd1f3bd0a02e77f47d05c96
Penedov <163603238+Penedov@users.noreply.github.com>
2024-05-22 18:56:28 -0300

    "Introduzindo o conhecimento básico sobre String em Python"

---
+++ Studies/Dia_03.py
@@ -0,0 +1,16 @@
+#Introdução a Python, Dia 01
+
+#Aprendendo sobre o tipo string , strings são o mesmo que matrizes aqui ou poem receber tal cargo
+
+A = "Hello world, estranho"
+print(A[13])
+print(A[13:14])
+
+for A in "Hello world, estranho":
+    print(A)
+    print(len(A))
+
+#Usando a lógica para buscar palavras ou confirmar palavras
+
+print("estranho" in A)
+print("Incomum" not in A)
